3a9822d3586501115f89d89a437c061efddcf29a
gitgregoryjones <gregory.jones@turner.com>
2024-10-10 12:43:47 -0400

    Update map.js

--- map.js
+++ map.js
@@ -91,7 +91,7 @@ function showDialog(message) {
       // Set the value of the input field
       if(document.getElementById('datetime')){
       	document.getElementById('datetime').value = formattedDateTime;
-	selectedDateTime = formattedTime;
+	selectedDateTime = formattedDateTime;
       }
     }
 
